38fcdbb07a781d0a39cae913429bcb2fb0d6ce44
Kevin Cipolla <k2cipolla@gmail.com>
2017-04-12 21:56:04 -0400

    Fixed several issues, and app is now working

--- finalproject.py
+++ finalproject.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
-from flask_wtf  import Form
+from flask import Flask, render_template, request, url_for, redirect
+from flask_wtf  import FlaskForm
+from flask_wtf.csrf import CSRFProtect
+from flask_bootstrap import Bootstrap
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dbsetup import Base, Restaurant, Menu
@@ -7,11 +9,12 @@ from dbsetup import Base, Restaurant, Menu
 from restaurantforms import NewRestaurantForm, EditRestaurantForm, DeleteRestaurantForm, NewMenuItemForm, EditMenuItemForm, DeleteMenuItemForm
 
 app = Flask(__name__)
+app.secret_key = '&\xe1`\x0c\xadA \xc7\xe9C\xab\xd3\x9e\x9aQI\x9aJh\xc0\xdf\xb6\x05l'
+Bootstrap(app)
+csrf = CSRFProtect()
+csrf.init_app(app)
 
 engine = create_engine('sqlite:///restaurantmenu.db')
-#Base.metadata.create_all(engine)
-#Base.metadata.bind(engine)
-
 DBSession = sessionmaker(bind=engine)
 
 @app.route('/')
@@ -24,7 +27,7 @@ def showRestaurants():
 @app.route('/restaurants/new/', methods=['GET', 'POST'])
 def restaurantsNew():
     session = DBSession()
-    form = NewRestaurantForm(request.POST)
+    form = NewRestaurantForm()
     if request.method == 'POST' and form.validate():
         newRestaurant = Restaurant(name=form.name.data)
         session.add(newRestaurant)
@@ -35,7 +38,7 @@ def restaurantsNew():
 @app.route('/restaurants/<int:restaurant_id>/edit/', methods=['GET', 'POST'])
 def restaurantsEdit(restaurant_id):
     session = DBSession()
-    form = NewRestaurantForm(request.POST)
+    form = EditRestaurantForm()
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     if request.method == 'POST' and form.validate():
         restaurant.name = form.name.data
@@ -47,7 +50,7 @@ def restaurantsEdit(restaurant_id):
 @app.route('/restaurants/<int:restaurant_id>/delete/', methods=['GET', 'POST'])
 def restaurantsDelete(restaurant_id):
     session = DBSession()
-    form = DeleteRestaurantForm(request.POST)
+    form = DeleteRestaurantForm()
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     if request.method == 'POST' and form.validate():
         if form.submit.data:
@@ -56,31 +59,31 @@ def restaurantsDelete(restaurant_id):
         return redirect(url_for('showRestaurants'))
     return render_template('deleterestaurant.html', restaurant = restaurant, form = form)
 
-@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/')
-def restaurantsMenu(restaurant_id, menu_id):
+@app.route('/restaurants/<int:restaurant_id>/menu/')
+def restaurantsMenu(restaurant_id):
     session = DBSession()
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     items = session.query(Menu).filter_by(id=restaurant_id).all()
     session.commit()
     return render_template('menu.html', restaurant = restaurant, items = items)
 
-@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/new/', methods=['GET', 'POST'])
-def restaurantsMenuItemNew(restaurant_id, menu_id):
+@app.route('/restaurants/<int:restaurant_id>/menu/new/', methods=['GET', 'POST'])
+def restaurantsMenuItemNew(restaurant_id):
     session = DBSession()
-    form = NewMenuItemForm(request.POST)
+    form = NewMenuItemForm()
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     if request.method == 'POST' and form.validate():
         items = session.query(Menu).filter_by(id=restaurant_id).all()
-        newMenuItem = Menu(name=form.name.data, description=form.description.data, price = form.price.data, restaurant_id=restaurant_id)
+        newMenuItem = Menu(item_name=form.name.data, item_description=form.description.data, item_price = form.price.data, restaurant_id=restaurant_id)
         session.add(newMenuItem)
         session.commit()
-        return redirect(url_for('showRestaurants'))
+        return redirect(url_for('restaurantsMenu',  restaurant_id=restaurant_id))
     return render_template('newmenuitem.html', restaurant = restaurant, form = form)
 
 @app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/edit/', methods=['GET', 'POST'])
 def restaurantsMenuEdit(restaurant_id, menu_id):
     session = DBSession()
-    form = EditMenuItemForm(request.POST)
+    form = EditMenuItemForm()
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     item = session.query(Menu).filter_by(id=menu_id).one()
     if request.method == 'POST' and form.validate():
@@ -92,19 +95,20 @@ def restaurantsMenuEdit(restaurant_id, menu_id):
             item.price = form.description.price
         session.add(item)
         session.commit()
-        return redirect(url_for('showRestaurants'))
+        return redirect(url_for('restaurantsMenu', restaurant_id=restaurant_id))
     return render_template('editmenuitem.html', restaurant = restaurant, form = form, item = item)
 
 @app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/delete/', methods=['GET', 'POST'])
 def restaurantsMenuDelete(restaurant_id, menu_id):
     session = DBSession()
-    form = DeleteMenuForm(request.POST)
+    form = DeleteMenuItemForm()
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     item = session.query(Menu).filter_by(id=menu_id).one()
     if request.method == 'POST' and form.validate():
         if form.submit.data:
             session.delete(item)
             session.commit()
-        return redirect(url_for('showRestaurants'))
+        return redirect(url_for('restaurantsMenu', restaurant_id=restaurant_id))
     return render_template('deletemenuitem.html', restaurant = restaurant, form = form, item = item)
 
 if __name__ == '__main__':
--- restaurantforms.py
+++ restaurantforms.py
@@ -1,29 +1,29 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, DecimalField
 from wtforms.validators import Required
 
-class NewRestaurantForm(Form):
+class NewRestaurantForm(FlaskForm):
     name = StringField('Enter name for new restaurant', validators=[Required()])
     submit = SubmitField('Create')
 
-class EditRestaurantForm(Form):
+class EditRestaurantForm(FlaskForm):
     name = StringField('Enter desired name', validators=[Required()])
     submit = SubmitField('Edit')
 
-class DeleteRestaurantForm(Form):
+class DeleteRestaurantForm(FlaskForm):
     submit = SubmitField('Delete')
 
-class NewMenuItemForm(Form):
+class NewMenuItemForm(FlaskForm):
     name = StringField('Enter name for new menu item', validators=[Required()])
     description = StringField('Enter description for new menu item', validators=[Required()])
     price = DecimalField('Enter price for new menu item', validators=[Required()])
     submit = SubmitField('Create')
 
-class EditMenuItemForm(Form):
+class EditMenuItemForm(FlaskForm):
     name = StringField('Enter desired name')
     description = StringField('Enter new description')
     price = DecimalField('Enter new price')
     submit = SubmitField('Edit')
 
-class DeleteMenuItemForm(Form):
+class DeleteMenuItemForm(FlaskForm):
     submit = SubmitField('Delete')
