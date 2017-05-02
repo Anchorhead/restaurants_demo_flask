40409c03ba4bc13a185d7219a11544d92ecf8bd0
Kevin Cipolla <k2cipolla@gmail.com>
2017-05-02 17:12:18 -0400

    fixed tablenames in dbsetup.py

--- dbsetup.py
+++ dbsetup.py
@@ -8,15 +8,15 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 class Restaurant(Base):
-    __tablename__ = 'restaurant'
+    __tablename__ = 'restaurants'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
 class Menu_Item(Base):
-    __tablename__ = 'menu'
+    __tablename__ = 'menu_items'
     id = Column(Integer, primary_key=True)
     item_name = Column(String(250), nullable=False)
     item_description = Column(String(250), nullable=False)
     item_price = Column(Numeric, nullable=False)
-    restaurant_id = Column(Integer, ForeignKey('restaurant.restaurant_id'))
+    restaurant_id = Column(Integer, ForeignKey('restaurants.restaurant_id'))
     restaurant = relationship(Restaurant)
